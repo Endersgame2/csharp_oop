37a185ba40a26762c0170c55875e60099ba26ae6
Manprit Kaur <manpritkaur@Manprits-MacBook-Air.local>
2022-08-07 13:11:11 -0500

    Connected all assets to the Building.cs class.

--- Building.cs
+++ Building.cs
@@ -7,6 +7,17 @@ namespace csharp_oop
         public string OuterColor { get; set; }
         public string InnerColor { get; set; }
         
+        public SecurityDoor SecureEntrance { get; set; }
+        public Rooms BasicRoom { get; set; }
+        public List<Washroom> Washrooms { get; set; }
+        public List<Rooms> Room { get; set; }
+        public Reception_Area Reception_Areas { get; set; }
+        public List<LockerRoom> LockerRooms { get; set; }
+        public LeisureArea LeisureAreas { get; set; }
+        public List<Water> WaterFountains { get; set; }
+        public Lights Light {get; set;}
+        public Fans Fan {get; set; }
+
         public Building(
             string userInputForOuterColor,
             string userInputForInnerColor,
@@ -16,6 +27,66 @@ namespace csharp_oop
         {
             OuterColor = userInputForOuterColor;
             InnerColor = userInputForInnerColor;
+       
+            SecureEntrance = new SecurityDoor(2, "Standard", "Secure Entrance", 10000);
+            
+
+            // Washrooms
+
+            Washrooms = new List<Washroom>();
+            Washroom maleWashroom = new Washroom(3, "Red", "Male Washroom", 20000);
+            Washroom femaleWashroom = new Washroom(3, "Pink", "Female Washroom", 20000);
+
+            Washrooms.Add(maleWashroom);
+            Washrooms.Add(femaleWashroom);
+
+            // Rooms
+            // needs to add eqipments to rooms
+
+
+            Room = new List<Rooms>();
+            Rooms BasicRoom = new Rooms(1, "Yellow", "Basic Room", 240000);
+            Rooms TreadmillRoom = new Rooms(1, "Wood", "Treadmill Room", 60000);
+            Rooms SpaRoom = new Rooms(1, "Black", "Spa Room", 40000);
+
+            Room.Add(BasicRoom);
+            Room.Add(TreadmillRoom);
+            Room.Add(SpaRoom);
+
+            //Reception Area
+
+            Reception_Areas = new Reception_Area("Reception Desk", "Creative", "Reception Room", 324000);
+
+            // Locker Room
+
+            LockerRooms = new List<LockerRoom>();
+            LockerRoom MaleLockerRoom = new LockerRoom(15, "Brown", "Dark Grey", "Male Locker Room", 23009);
+            LockerRoom FemaleLockerRoom = new LockerRoom(15, "Brown", "White", "Female Locker Room", 23500);
+
+            LockerRooms.Add(MaleLockerRoom);
+            LockerRooms.Add(FemaleLockerRoom);
+
+            // Leisure Area
+
+            LeisureAreas = new LeisureArea("Couch", "White", "Leisure Area", 345600);
+
+            // Water Fountains
+
+            WaterFountains = new List<Water>();
+            Water MaleWaterFountain = new Water(2, "Core", "Male Water Fountain", 4000);
+            Water FemaleWaterFountain = new Water(2, "Fresh", "Female Water Fountain", 4000);
+
+            WaterFountains.Add(MaleWaterFountain);
+            WaterFountains.Add(FemaleWaterFountain);
+
+
+            // Lights
+
+            Light = new Lights(30, "Yellow and White", "Lights", 20);
+
+            // Fans
+
+            Fan = new Fans(10, "White", "Fans", 200);
         }
     }
 }
