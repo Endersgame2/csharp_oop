809eb553aacb40aafffee1b078e7ecc2e5a94798
Manprit Kaur <manpritkaur@Manprits-MacBook-Air.local>
2022-08-07 13:08:04 -0500

    Added gym assets interface and abstract classes.

---
+++ Asset.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace csharp_oop
+{
+    public interface Asset
+    {
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+    }
+}
+
---
+++ GymAsset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace csharp_oop
+{
+    public abstract class GymAsset
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+
+
+        public GymAsset(string nameOfAsset, decimal valueOfAsset)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = nameOfAsset;
+            Value = valueOfAsset;
+        }
+    }
+}
+
